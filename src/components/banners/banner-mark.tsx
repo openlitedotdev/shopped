4495d73e385ba0258fc456b96d3d300e5baeea84
Gerzon05 <gerzonarletrangel@gmail.com>
2024-09-14 15:13:20 -0500

    feat: a folder was created for the banner component

---
+++ src/components/banners/banner-mark.tsx
@@ -0,0 +1,8 @@
+export default function BannerMark() {
+  return (
+    <div className="bg-blue-500 text-white p-4">
+      <h2 className="text-xl">Banner Mark</h2>
+      <p>Get 50% off on all items</p>
+    </div>
+  )
+}
