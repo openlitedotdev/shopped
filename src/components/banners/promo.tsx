2afebff95c7f9f7fcaf94e6dd2cc9b84d11d1b89
castrogarciajs <johancs.mm@gmail.com>
2024-09-14 15:24:41 -0500

    feat: create banner promo component

---
+++ src/components/banners/promo.tsx
@@ -0,0 +1,9 @@
+export default function Promo() {
+  return (
+    <div className="bg-default py-3 text-center text-default-foreground">
+      <p className="hover:underline">
+        Sign up and get 20% off to your first order. Sign Up Now
+      </p>
+    </div>
+  )
+}
