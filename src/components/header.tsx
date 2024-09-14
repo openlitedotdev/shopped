39124c6022987282c6ab17ff9722db69cb417ffb
castrogarciajs <johancs.mm@gmail.com>
2024-09-13 23:27:29 -0500

    feat: icon folder powered by hugeicons

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,31 @@
+import { Input } from '@openlite/ui'
+import { Link } from 'wouter'
+import { StoreIcon } from './icons/store'
+
+const menus = [
+  { name: 'Shop', path: '/categories' },
+  { name: 'On Sale', path: '#' },
+  { name: 'New Arrivals', path: '#' },
+  { name: 'Brands', path: '#' },
+]
+
 export default function Header() {
   return (
-    <footer>
-      <h2>Hola header</h2>
-    </footer>
+    <header className="flex items-center justify-between bg-white px-8 py-5 shadow">
+      <Link to="/">
+        <img src="/logo.png" alt="logo official for shopped" className="w-44" />
+      </Link>
+      <nav>
+        {menus.map((menuItem) => (
+          <Link to={menuItem.path}>{menuItem.name}</Link>
+        ))}
+      </nav>
+
+      <Input placeholder="Search for products..." />
+
+      <Link to="/storage">
+        <StoreIcon />
+      </Link>
+    </header>
   )
 }
