a1d7685e744f1bcaa5cbcebe7a8e98362109ce58
Gerzon05 <gerzonarletrangel@gmail.com>
2024-09-14 23:46:34 -0500

    feat: new comment section

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,5 +1,6 @@
 import BannerMark from '@/components/banners/banner-mark'
-import CategoriesStyle from '@/components/Categories-style'
+import CategoriesStyle from '@/components/categories-style'
+import Comments from '@/components/comments'
 import Hero from '@/components/hero'
 import NewArrivals from '@/components/new-arrivals'
 import TopSelling from '@/components/top-selling'
@@ -17,6 +18,7 @@ export default function Home() {
       </div>
       <TopSelling />
       <CategoriesStyle />
+      <Comments />
     </>
   )
 }
