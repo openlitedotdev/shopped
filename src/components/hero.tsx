74e0dd8c2d5cd3a499702576b8689bc2ce396765
sebastiandotdev <johancs.mm@gmail.com>
2024-09-26 14:33:24 -0500

    perf: improve download image with picture

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,8 +1,7 @@
 import StatCars from '@/components/stat-card'
 import { Button } from '@openlite/ui'
-import { Suspense, lazy } from 'react'
 
-const LazyHeroImage = lazy(() => import('./hero-image.lazy'))
+import LazyHeroImage from './hero-image.lazy'
 
 const StatCard = [
   {
@@ -44,32 +43,8 @@ export default function Hero() {
           ))}
         </article>
       </div>
-      <Suspense fallback={<SuspenseHeroImage />}>
-        <LazyHeroImage />
-      </Suspense>
-    </section>
-  )
-}
 
-function SuspenseHeroImage() {
-  return (
-    <div className="relative aspect-video h-[478px] w-full animate-pulse bg-gray-200">
-      <div className="absolute inset-0 flex items-center justify-center">
-        <svg
-          className="h-12 w-12 text-gray-400"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
-          ></path>
-        </svg>
-      </div>
-    </div>
+      <LazyHeroImage />
+    </section>
   )
 }
